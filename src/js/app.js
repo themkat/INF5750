454fe404ff0a19860b82b33de361167e6cd42ed0
Marie Katrine <someemail@email.email>
2014-11-12 18:21:43 +0100

    Added a rudimentary list of courses.

--- src/js/app.js
+++ src/js/app.js
@@ -15,5 +15,6 @@ config(['$routeProvider', function($routeProvider , RestangularProvider) {
     $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
     $routeProvider.when('/view3', {templateUrl: 'partials/partial3.html', controller: 'MyCtrl3'});
     $routeProvider.when('/qm', {templateUrl: 'partials/quizmaker.html', controller: 'MyCtrl1'});
+     $routeProvider.when('/courses', {templateUrl: 'partials/courses.html', controller: 'MyCtrl1'});
     $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
