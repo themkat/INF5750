f6607b872f2e9436e12971096081e577b60f0e5a
Martin <martirj@ifi.uio.no>
2014-11-12 15:54:37 +0100

    Added example code.

---
+++ src/js/controllers.js
@@ -0,0 +1,249 @@
+'use strict';
+
+/* Controllers */
+
+/*  It has become considered better practise to separate controllers into
+    different files. Not like it's done here. See angular-seed for an example
+    of how it's done (this is based on angular-seed one year ago.
+
+    Note that when you get an object from a $resource, this object
+    automatically gets some $get/$save methods that can use if you want to
+    update or save the object again onto the server.
+
+    See: https://docs.angularjs.org/api/ngResource/service/$resource for info
+*/
+
+angular.module('myApp.controllers', []).
+    controller('MyCtrl1', ['$scope', 'MeService', 'ProfileService',
+        function ($scope, MeService, ProfileService) {
+
+        $scope.dhisAPI = dhisAPI;
+
+        $scope.me = MeService.get(function () {
+            console.log('$scope.me='+JSON.stringify($scope.me));
+        });
+
+        $scope.refreshMe = function() {
+            $scope.me.$get();
+        };
+
+        $scope.profile = ProfileService.get(function () {
+            console.log('$scope.profie='+JSON.stringify($scope.profile));
+        });
+
+        $scope.saveProfile = function() {
+            $scope.profile.$save({}, function() {
+                    alert("Profile saved successfully.");
+                },
+                function() {
+                    alert("Profile save failed.");
+                }
+            );
+            console.log('$scope.profie='+JSON.stringify($scope.profile));
+        };
+    }])
+    .controller('MyCtrl2', ['$scope', 'UserSettingService', function ($scope, UserSettingService) {
+
+        $scope.userSetting = UserSettingService.get(function () {
+            console.log("$scope.userSetting="+JSON.stringify($scope.userSetting));
+        });
+
+        $scope.saveSetting = function () {
+            console.log('Saving setting:'+JSON.stringify($scope.userSetting));
+            $scope.userSetting.$save({}, function() {
+                alert("Data saved successfully.");
+            });
+        }
+
+        $scope.refreshSetting = function () {
+            $scope.userSettingFetched = UserSettingService.get(function () {
+                $scope.earlierSetting = $scope.userSettingFetched.value;
+            });
+        }
+
+    }])
+    .controller('MyCtrl3', ['$scope', function ($scope) {
+        console.log('Ctrl3');
+
+        $scope.location = {lat: 0.602118, lng: 30.160217};
+
+        $scope.center = {
+            lat: 0.577400,
+            lng: 30.201073,
+            zoom: 10
+        };
+
+        $scope.markers = new Array();
+
+        $scope.addMarkers = function () {
+            console.log('Ctrl3 Add markers');
+            $scope.markers.push({
+                lat: $scope.location.lat,
+                lng: $scope.location.lng,
+                message: "My Added Marker"
+            });
+
+        };
+
+        $scope.$on("leafletDirectiveMap.click", function (event, args) {
+            var leafEvent = args.leafletEvent;
+            console.log('Ctrl3 adding marker at lat=' + leafEvent.latlng.lat + ', lng=' + leafEvent.latlng.lng);
+            $scope.location.lng = leafEvent.latlng.lng;
+            $scope.location.lat = leafEvent.latlng.lat;
+
+            $scope.markers.push({
+                lat: leafEvent.latlng.lat,
+                lng: leafEvent.latlng.lng,
+                message: "My Added Marker"
+            });
+        });
+
+        $scope.removeMarkers = function () {
+            console.log('Ctrl3 remove markers');
+            $scope.markers = new Array();
+        }
+
+        // Add an initial marker
+        $scope.markers.push({
+            lat: $scope.location.lat,
+            lng: $scope.location.lng,
+            focus: true,
+            message: "A draggable marker",
+            draggable: true
+        });
+
+        $scope.removeOsmLayer = function() {
+            delete this.layers.baselayers.osm;
+            delete this.layers.baselayers.googleTerrain;
+            delete this.layers.baselayers.googleRoadmap;
+            delete this.layers.baselayers.googleHybrid;
+            this.layers.baselayers.cycle = {
+                name: 'OpenCycleMap',
+                type: 'xyz',
+                url: 'http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png',
+                layerOptions: {
+                    subdomains: ['a', 'b', 'c'],
+                    attribution: '&copy; <a href="http://www.opencyclemap.org/copyright">OpenCycleMap</a> contributors - &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                    continuousWorld: true
+                }
+            };
+        };
+
+        $scope.addOsmLayer = function() {
+            delete this.layers.baselayers.cycle;
+            delete this.layers.baselayers.googleTerrain;
+            delete this.layers.baselayers.googleRoadmap;
+            delete this.layers.baselayers.googleHybrid;
+            this.layers.baselayers.osm = {
+                name: 'OpenStreetMap',
+                type: 'xyz',
+                url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                layerOptions: {
+                    subdomains: ['a', 'b', 'c'],
+                    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                    continuousWorld: true
+                }
+            };
+        };
+
+        $scope.showGoogleLayers = function() {
+            delete this.layers.baselayers.cycle;
+            delete this.layers.baselayers.osm;
+            this.layers.baselayers = {
+                googleTerrain: {
+                    name: 'Google Terrain',
+                    layerType: 'TERRAIN',
+                    type: 'google'
+                },
+                googleHybrid: {
+                    name: 'Google Hybrid',
+                    layerType: 'HYBRID',
+                    type: 'google'
+                },
+                googleRoadmap: {
+                    name: 'Google Streets',
+                    layerType: 'ROADMAP',
+                    type: 'google'
+                }
+            };
+        };
+
+        angular.extend($scope, {
+            layers: {
+                baselayers: {
+                    osm: {
+                        name: 'OpenStreetMap',
+                        type: 'xyz',
+                        url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                        layerOptions: {
+                            subdomains: ['a', 'b', 'c'],
+                            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                            continuousWorld: true
+                        }
+                    },
+                    cycle: {
+                        name: 'OpenCycleMap',
+                        type: 'xyz',
+                        url: 'http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png',
+                        layerOptions: {
+                            subdomains: ['a', 'b', 'c'],
+                            attribution: '&copy; <a href="http://www.opencyclemap.org/copyright">OpenCycleMap</a> contributors - &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                            continuousWorld: true
+                        }
+                    },
+                    googleTerrain: {
+                        name: 'Google Terrain',
+                        layerType: 'TERRAIN',
+                        type: 'google'
+                    },
+                    googleHybrid: {
+                        name: 'Google Hybrid',
+                        layerType: 'HYBRID',
+                        type: 'google'
+                    },
+                    googleRoadmap: {
+                        name: 'Google Streets',
+                        layerType: 'ROADMAP',
+                        type: 'google'
+                    }/*,
+                    landscape: {
+                        name: 'Landscape',
+                        type: 'xyz',
+                        url: 'http://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png',
+                        layerOptions: {
+                            subdomains: ['a', 'b', 'c'],
+                            attribution: '&copy; <a href="http://www.thunderforest.com/about/">Thunderforest</a> contributors - &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                            continuousWorld: true
+                        }
+                    },
+                    cloudmade1: {
+                        name: 'Cloudmade Night Commander',
+                        type: 'xyz',
+                        url: 'http://{s}.tile.cloudmade.com/{key}/{styleId}/256/{z}/{x}/{y}.png',
+                        layerParams: {
+                            key: '007b9471b4c74da4a6ec7ff43552b16f',
+                            styleId: 999
+                        },
+                        layerOptions: {
+                            subdomains: ['a', 'b', 'c'],
+                            continuousWorld: true
+                        }
+                    },
+                    cloudmade2: {
+                        name: 'Cloudmade Tourist',
+                        type: 'xyz',
+                        url: 'http://{s}.tile.cloudmade.com/{key}/{styleId}/256/{z}/{x}/{y}.png',
+                        layerParams: {
+                            key: '007b9471b4c74da4a6ec7ff43552b16f',
+                            styleId: 7
+                        },
+                        layerOptions: {
+                            subdomains: ['a', 'b', 'c'],
+                            continuousWorld: true
+                        }
+                    }*/
+                }
+            }
+        });
+    }]);
+
---
+++ src/js/services.js
@@ -0,0 +1,59 @@
+'use strict';
+
+/* It has become considered better practise to separate services into
+ different files. Not like it's done here. See angular-seed for an example
+ of how it's done (this is based on angular-seed one year ago.
+ */
+
+/* Services */
+
+var myAppServices = angular.module('myApp.services', ['ngResource']);
+
+myAppServices.factory("MeService", function ($resource) {
+    return $resource(
+        dhisAPI+'/api/me',
+        {
+            // If you're passing variables, for example into the URL
+            // they would be here and then as :varName in the URL
+        },
+        {
+            // If you want to implement special functions, you'd
+            // put them here.
+        }
+    );
+});
+
+myAppServices.factory("ProfileService", function ($resource) {
+    return $resource(
+            dhisAPI+'/api/me/profile',
+        {
+            // If you're passing variables, for example into the URL
+            // they would be here and then as :varName in the URL
+        },
+        {
+            // If you want to implement special functions, you'd
+            // put them here.
+        }
+    );
+});
+
+
+
+myAppServices.factory("UserSettingService", function ($resource) {
+    return $resource(
+            dhisAPI+'/api/userSettings/exampleapp.usersetting',
+        {
+            // If you're passing variables, for example into the URL
+            // they would be here and then as :varName in the URL
+        },
+        {
+            // If you want to implement special functions, you'd
+            // put them here. In this case, the content type cannot be
+            // JSON, so we need to change it to text/plain
+            save: {
+                method:'POST',
+                isArray:false,
+                headers: {'Content-Type': 'text/plain'}}
+        }
+    );
+});
