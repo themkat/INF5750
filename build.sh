b819a299a5944c80fcd9ad47b7ab890b08b2423d
Marie Katrine <someemail@email.email>
2014-11-12 17:43:45 +0100

    changed stuff

--- build.sh
+++ build.sh
@@ -1,9 +1,16 @@
 #!/bin/env bash
 
-PACKAGE="quiz.zip"
+CURDIR=$(pwd)
+SRCDIR="./src/"
+PACKAGE="$CURDIR/quiz.zip"
+
+cd "$SRCDIR"
+
 printf "[ .. ] Cleaning environment"
 rm -f "$PACKAGE"
 printf "\r%s\n" "[ ok ]"
 printf "[ .. ] Building application package %s" "$PACKAGE"
-zip -r "$PACKAGE" ./src/* > /dev/null 2>&1
+zip -r "$PACKAGE" ./* > /dev/null 2>&1
 printf "\r%s\n" "[ ok ]"
+
+cd "$CURDIR"
