f141f36f49a0eb7d98625f8a2247935bc95e8878
vebjorrs <vebjorrs@ifi.uio.no>
2014-11-18 11:46:39 +0100

    Removed example code, scarcely increased the usefulness of courses.html

--- src/js/app.js
+++ src/js/app.js
@@ -11,10 +11,7 @@ angular.module('myApp', [
   'leaflet-directive'
 ]).
 config(['$routeProvider', function($routeProvider , RestangularProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-    $routeProvider.when('/view3', {templateUrl: 'partials/partial3.html', controller: 'MyCtrl3'});
     $routeProvider.when('/qm', {templateUrl: 'partials/quizmaker.html', controller: 'MyCtrl1'});
      $routeProvider.when('/courses', {templateUrl: 'partials/courses.html', controller: 'MyCtrl1'});
-    $routeProvider.otherwise({redirectTo: '/view1'});
+    $routeProvider.otherwise({redirectTo: '/courses'});
 }]);
