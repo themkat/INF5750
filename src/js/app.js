0a1c89e0a5ea9e4d087fb3afe4826b78e3743f2e
Marie Katrine Ekeberg <mariuek@ifi.uio.no>
2014-11-18 17:35:56 +0100

    Created a course-view for the user

--- src/js/app.js
+++ src/js/app.js
@@ -13,5 +13,8 @@ angular.module('myApp', [
 config(['$routeProvider', function($routeProvider , RestangularProvider) {
     $routeProvider.when('/qm', {templateUrl: 'partials/quizmaker.html', controller: 'MyCtrl1'});
      $routeProvider.when('/courses', {templateUrl: 'partials/courses.html', controller: 'MyCtrl1'});
+
+    $routeProvider.when('/ucourses', {templateUrl: 'partials/user-courses.html', controller: 'MyCtrl1'});
+
     $routeProvider.otherwise({redirectTo: '/courses'});
 }]);
