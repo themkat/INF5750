a6bdf819d94d65d3e20ca888d062112f7b72ccdd
vebjorrs <vebjorrs@ifi.uio.no>
2014-11-19 14:11:01 +0100

    Worked to make quiz.html accessible from the app.

--- src/js/app.js
+++ src/js/app.js
@@ -13,8 +13,8 @@ angular.module('myApp', [
 config(['$routeProvider', function($routeProvider , RestangularProvider) {
     $routeProvider.when('/qm', {templateUrl: 'partials/quizmaker.html', controller: 'MyCtrl1'});
      $routeProvider.when('/courses', {templateUrl: 'partials/courses.html', controller: 'MyCtrl1'});
-
     $routeProvider.when('/ucourses', {templateUrl: 'partials/user-courses.html', controller: 'MyCtrl1'});
+	$routeProvider.when('/quiz', {templateUrl: 'partials/quiz.html', controller: 'MyCtrl1'});
 
     $routeProvider.otherwise({redirectTo: '/courses'});
 }]);
