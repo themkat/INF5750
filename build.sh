3969081f50c0b0e58ec22d2dd94f5e3090367c02
Martin <martirj@ifi.uio.no>
2014-11-19 16:20:28 +0100

    Added check for the zip program.

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/env bash
 
+if ! hash zip 2>/dev/null; then
+	printf "\r[ !! ] program zip not found\n"
+	exit 1
+fi
+
 # Project settings.
 PACKAGE_NAME="quiz"
 
@@ -10,7 +15,7 @@ PROJECT_DIRECTORY="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Discover related project paths.
 CURRENT_DIRECTORY=$(pwd)
 PACKAGE_PATH="$CURRENT_DIRECTORY/$PACKAGE_NAME.zip"
-SOURCE_DIRECTORY="$PROJECT_DIRECTORY/src/"
+SOURCE_DIRECTORY="$PROJECT_DIRECTORY"
 
 # Temporarily switch to the source directory.
 cd "$SOURCE_DIRECTORY"
@@ -18,14 +23,14 @@ cd "$SOURCE_DIRECTORY"
 # Remove previously generated zip archive.
 if [ -f "$PACKAGE_PATH" ];
 then
-	MESSAGE=$(printf "Removing package %s" "$PACKAGE_PATH")
+	MESSAGE=$(printf "removing package %s" "$PACKAGE_PATH")
 	printf "[ .. ] $MESSAGE"
 	rm -f "$PACKAGE_PATH"
 	printf "\r[ ok ] $MESSAGE\n"
 fi
 
 # Build zip archive.
-MESSAGE=$(printf "Building package %s" "$PACKAGE_PATH")
+MESSAGE=$(printf "building package %s" "$PACKAGE_PATH")
 printf "[ .. ] $MESSAGE"
 zip -r "$PACKAGE_PATH" ./* > /dev/null 2>&1
 printf "\r[ ok ] $MESSAGE\n"
